d89552b0ae86d6de45e0cee99b526837f3d8930b
liuqian0057 <63543903+liuqian0057@users.noreply.github.com>
2020-04-12 21:52:39 +0800

    GIT初始化.py
    使用GIT设置用户名和邮箱，创建文件等

---
+++ GIT初始化.py
@@ -0,0 +1,6 @@
+设置用户名  git config --global user.name 'liuqian0057'
+设置用户名邮箱 git config --global user.email 'liuqian0057@163.com'
+创建文件夹aggd       mkdir aggd
+生成隐藏文件存储信息       cd aggd
+                         git init
+创建文件 touch ads.php
